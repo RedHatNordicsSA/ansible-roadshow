45cdc57f8e70b9ad72a6a987b8e4986ba76692dc
Magnus Glantz <sudo@redhat.com>
2018-08-27 14:17:34 +0200

    Update host-prep.sh

--- content/host-prep.sh
+++ content/host-prep.sh
@@ -11,6 +11,15 @@ if [ ! -d /root/.ssh ]; then
   chmod 600 /root/.ssh
 fi
 
+useradd student
+echo "RHforum18Pass" | passwd student --stdin
+mkdir /home/student/.ssh
+chmod 700 /home/student/.ssh
+curl https://raw.githubusercontent.com/mglantz/ansible-roadshow/master/content/id_rsa >/home/student/.ssh/id_rsa
+curl https://raw.githubusercontent.com/mglantz/ansible-roadshow/master/content/id_rsa.pub >/home/student/.ssh/authorized_keys
+chmod 600 /home/student/.ssh/*
+chown student:student /home/student/.ssh -R
+
 # Create ssh key
 cat << 'EOF' >/root/.ssh/authorized_keys
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCR/+C0do9cM9edqeG8y81HQHmPAiviI3s2HDhLPMfSZ9iqxwS/2Cpt1TDfVVh+Qoo3mVgSe6Y7jlX6TNeG2aFQfCG33/3l3OZaACheAzJZ+vsL+OnIMT8sdzkqkYPeE5yeuutsDzdPj9a6nr/HIn6/BV2GFHCqxP7NswW7v1q8n56ucMto+GU2Ih0pIkEOIbjdQ83npRtukeTtPBIBVQJY7XI6bnbf7m6dJ5sAKe94EnOCqR3h7p19+mrmDNXi7vnWBFIu6ctfsUXEHDewGny6b3A/UKk5x1EGsIPuZkbdXBKDF9nw39171jldawiEN3XLQbG2mcrZ6l/cVlV8be0D
