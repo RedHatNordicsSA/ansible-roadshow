7f8d9e2823e99073a7266eb05982317188dc5f39
Magnus Glantz <sudo@redhat.com>
2018-08-27 11:14:12 +0200

    Basic editors in place

--- content/host-prep.sh
+++ content/host-prep.sh
@@ -3,7 +3,7 @@
 # Prep hosts to be managed by Tower
 
 yum -y install http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum -y install python2-pip wget bind-utils ansible
+yum -y install python2-pip wget bind-utils ansible nano vim screen emacs joe
 
 # Create directory for ssh public key
 if [ ! -d /root/.ssh ]; then
@@ -21,5 +21,16 @@ mkdir /root/.ssh
 chmod 700 /root/.ssh
 curl https://raw.githubusercontent.com/mglantz/ansible-roadshow/master/content/id_rsa.pub >/root/.ssh/authorized_keys
 chmod 600 /root/.ssh/*
+
+cat << 'EOF' >/home/student/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCR/+C0do9cM9edqeG8y81HQHmPAiviI3s2HDhLPMfSZ9iqxwS/2Cpt1TDfVVh+Qoo3mVgSe6Y7jlX6TNeG2aFQfCG33/3l3OZaACheAzJZ+vsL+OnIMT8sdzkqkYPeE5yeuutsDzdPj9a6nr/HIn6/BV2GFHCqxP7NswW7v1q8n56ucMto+GU2Ih0pIkEOIbjdQ83npRtukeTtPBIBVQJY7XI6bnbf7m6dJ5sAKe94EnOCqR3h7p19+mrmDNXi7vnWBFIu6ctfsUXEHDewGny6b3A/UKk5x1EGsIPuZkbdXBKDF9nw39171jldawiEN3XLQbG2mcrZ6l/cVlV8be0D
+EOF
+chmod 600 /home/student/.ssh/authorized_keys
+
+mkdir /root/.ssh
+chmod 700 /root/.ssh
+curl https://raw.githubusercontent.com/mglantz/ansible-roadshow/master/content/id_rsa.pub >/root/.ssh/authorized_keys
+chmod 600 /root/.ssh/*
+
 
 	
